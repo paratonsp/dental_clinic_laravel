e94377ba101dd0636127805bc99b17dced8aa1ce
paratonsp <paratonsp@gmail.com>
2024-10-29 01:03:57 +0700

    update: name

--- resources/views/auth/registrasi.blade.php
+++ resources/views/auth/registrasi.blade.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Dental Clinic</title>
+    <title>Molar Dental</title>
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/logo.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/molar-logo.png')}}">
     <link href="{{asset('css/style.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('vendor/toastr/css/toastr.min.css')}}">
 </head>
